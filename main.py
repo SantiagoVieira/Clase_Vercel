20f9d19617112a3fc5125cd5489eaed17349e9fe
Santiago_Vieira <svieira336@soyudemedellin.edu.co>
2024-10-02 19:11:46 -0500

    cuarto cambio

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ templates = Jinja2Templates(directory=".")
 @app.get("/", response_class=HTMLResponse)
 
 async def read_root(request: Request):
-        mensaje = "hola profe"
-        lista = [1, 2, 3, 4, 5, 6,7, 8]
+    mensaje = "hola profe"
+    lista = [1, 2, 3, 4, 5, 6,7, 8]
 
     return templates.TemplateResponse("index.html", {"request": request, "message": mensaje, "X": lista})
