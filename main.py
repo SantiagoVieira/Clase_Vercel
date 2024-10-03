7768211f545b61c8607cc92a4209936387ca250f
Santiago_Vieira <svieira336@soyudemedellin.edu.co>
2024-10-02 19:08:32 -0500

    tercer cambio

--- main.py
+++ main.py
@@ -22,4 +22,4 @@ async def read_root(request: Request):
         mensaje = "hola profe"
         lista = [1, 2, 3, 4, 5, 6,7, 8]
 
-    return templates.TemplateResponse("index.html", {"request": request, message: "mensaje", "X": lista})
+    return templates.TemplateResponse("index.html", {"request": request, "message": mensaje, "X": lista})
